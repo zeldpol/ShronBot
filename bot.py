f5de22172905323350e81117579ec2848e31c300
Dmitry Savich <dima@savich.me>
2023-01-31 20:01:06 +0200

    fix, refactoring

--- bot.py
+++ bot.py
@@ -1,12 +1,14 @@
 import os
 import telebot as tb
 from PIL import Image
+import io
 import requests
 
 
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 bot = tb.TeleBot(BOT_TOKEN)
 bot_info = bot.get_me()
+MAX_IMAGE_SIZE = 512
 welcome_text= "Welcome to the club, buddy! Send me a sticker from any pack and I'll create you your own personal pack. If you send me a sticker from this pack, I'll delete it. Enjoy"
 
 @bot.message_handler(commands=['start'])
@@ -16,74 +18,67 @@ def send_welcome(message):
 @bot.message_handler(content_types=["sticker"])
 def sticker(message):
     if message.sticker.is_animated:
-        pack_name = f"animated_{message.from_user.username}_by_{bot_info.username}"
         file_info = bot.get_file(message.sticker.file_id)
         downloaded_file = bot.download_file(file_info.file_path)
-        try:
-            bot.create_new_sticker_set(message.from_user.id, pack_name, pack_name, message.sticker.emoji, tgs_sticker = downloaded_file)
-        except:
-            print("1")
-        if bot.get_sticker_set(pack_name):
-            if message.sticker.set_name == pack_name:
-                bot.delete_sticker_from_set(message.sticker.file_id)
-                bot.send_message(message.chat.id, "Deleted!")
-            else:
-                bot.add_sticker_to_set(message.from_user.id, pack_name, message.sticker.emoji,tgs_sticker = downloaded_file)
-        else:
-            bot.create_new_sticker_set(message.from_user.id, pack_name, pack_name, message.sticker.emoji,tgs_sticker = downloaded_file)
-
+        add_or_delete_sticker(message, tgs_sticker=downloaded_file)
     else:
-        pack_name = f"static_{message.from_user.username}_by_{bot_info.username}"
-        try:
-            bot.create_new_sticker_set(message.from_user.id, pack_name, pack_name, message.sticker.emoji, message.sticker.file_id)
-        except:
-            print("2")
-        if bot.get_sticker_set(pack_name):
-            if message.sticker.set_name == pack_name:
-                bot.delete_sticker_from_set(message.sticker.file_id)
-                bot.send_message(message.chat.id, "Deleted!")
-            else:
-                bot.add_sticker_to_set(message.from_user.id, pack_name, message.sticker.emoji, message.sticker.file_id)
-        else:
-            bot.create_new_sticker_set(message.from_user.id, pack_name, pack_name, message.sticker.emoji, message.sticker.file_id)
+        add_or_delete_sticker(message, png_sticker=message.sticker.file_id)
 
-    bot.send_message(message.chat.id, f"t.me/addstickers/{pack_name}")
+
+def add_or_delete_sticker(message, tgs_sticker=None, png_sticker=None, emoji=None):
+    assert bool(tgs_sticker) != bool(png_sticker), "exactly one of tgs_sticker and png_sticker should be passed" 
+    if not emoji:
+        emoji = message.sticker.emoji
+    if message.sticker.set_name == get_pack_name(message):
+        del_sticker(message)
+    else:
+        add_sticker(message, tgs_sticker, png_sticker, emoji=emoji)
+
+
+def add_sticker(message, tgs_sticker, png_sticker, emoji):
+    pack_name = get_pack_name(message)
+    try:
+        bot.add_sticker_to_set(message.from_user.id, pack_name, emojis=emoji, tgs_sticker=tgs_sticker, png_sticker=png_sticker)
+        bot.send_message(message.chat.id, f"Sticker added! t.me/addstickers/{pack_name}")
+    except tb.apihelper.ApiTelegramException as e:
+        if e.description.strip() == 'Bad Request: STICKERSET_INVALID':
+            bot.create_new_sticker_set(message.from_user.id, pack_name, pack_name, emoji, tgs_sticker=tgs_sticker)
+            bot.send_message(message.chat.id, f"New stickerset created. Sticker added! t.me/addstickers/{pack_name}")
+        else:
+            raise e
+
+def get_pack_name(message):
+    if message.sticker and message.sticker.is_animated:
+        return f"animated_{message.from_user.username}_by_{bot_info.username}"    
+    return f"static_{message.from_user.username}_by_{bot_info.username}"
+
+def del_sticker(message):
+    bot.delete_sticker_from_set(message.sticker.file_id)
+    bot.send_message(message.chat.id, f"Deleted! t.me/addstickers/{get_pack_name(message)}")
 
 
 @bot.message_handler(content_types=["photo"])
 def image2sticker(message):
-    #print('message.photo =', message.photo)
     fileID = message.photo[-1].file_id
     file_info = bot.get_file(fileID)
-    #print('file.file_path =', file_info.file_path)
     downloaded_file = bot.download_file(file_info.file_path)
-    with open("tmp/image.png", 'wb') as new_file:
-        new_file.write(downloaded_file)
+    resized_image = resize(downloaded_file)
+    add_sticker(message, tgs_sticker=None, png_sticker=resized_image, emoji="😃")
 
-    image = Image.open('tmp/image.png')
+
+def resize(downloaded_file: bytes):
+    image = Image.open(io.BytesIO(downloaded_file))
     image_format = image.width/image.height
-    #print(image.width, image.height, image_format)
-    if image_format  == 1:
-        new_image = image.resize((512, 512))
+    if image.width ==image.height:
+        new_image = image.resize((MAX_IMAGE_SIZE, MAX_IMAGE_SIZE))
     elif image_format > 1:
-        new_image = image.resize((512, int(512/image_format)))
+        new_image = image.resize((MAX_IMAGE_SIZE, int(MAX_IMAGE_SIZE/image_format)))
     else:
-        new_image = image.resize((int(512*image_format), 512))
-    new_image.save('image_512.png')
-
-    with open('image_512.png', 'rb') as file:
-        upload_image = file.read()
-
-    pack_name = f"static_{message.from_user.username}_by_{bot_info.username}"
-    try:
-        bot.create_new_sticker_set(message.from_user.id, pack_name, pack_name, "😀", upload_image)
-    except:
-        print("3")
-    if bot.get_sticker_set(pack_name):
-        bot.add_sticker_to_set(message.from_user.id, pack_name, "😀", upload_image)
-    else:
-        bot.create_new_sticker_set(message.from_user.id, pack_name, pack_name, "😀", upload_image)
-    bot.send_message(message.chat.id, f"t.me/addstickers/{pack_name}")
+        new_image = image.resize((int(MAX_IMAGE_SIZE*image_format), MAX_IMAGE_SIZE))
+    img_byte_arr = io.BytesIO()
+    new_image.save(img_byte_arr, format='PNG')
+    return img_byte_arr.getvalue()
+     
 
 
 @bot.message_handler(content_types=['video_note'])
@@ -97,16 +92,16 @@ def vn2sticker(message):
     pack_name = f"nv_by_{bot_info.username}"
     with open("tmp/video_note.mp4", 'wb') as new_file:
         new_file.write(downloaded_file)
-    if os.system(f'ffmpeg -i tmp/video_note.mp4 -vf "crop={ln}:{ln},scale=512:512" -y tmp/output.mp4') != 0:
-        bot.send_message(message.chat.id, 'BONK, something wrong! *бунт.jpg*')
-    if os.system('ffmpeg -i tmp/output.mp4 -y -c:v libvpx-vp9 -crf 30 -b:v 0 -b:a 128k -loop 0 -an -c:a libopus tmp/output.webm') != 0:
-        bot.send_message(message.chat.id, 'BONK, something wrong! *бунт2.jpg*')
+    if code := os.system(f'ffmpeg -i tmp/video_note.mp4 -vf "crop={ln}:{ln},scale=512:512" -y tmp/output.mp4'):
+        bot.send_message(message.chat.id, f'BONK, something wrong! *бунт.jpg*, ffmpeg returned code {code}')
+    elif code := os.system('ffmpeg -i tmp/output.mp4 -y -c:v libvpx-vp9 -crf 30 -b:v 0 -b:a 128k -loop 0 -an -c:a libopus tmp/output.webm'):
+        bot.send_message(message.chat.id, f'BONK, something wrong! *бунт2.jpg*, ffmpeg returned code {code}')
+    else:
+        with  open('tmp/output.webm', 'rb') as video_file:
+            video = video_file.read()
+            bot.create_new_sticker_set(message.from_user.id, pack_name, pack_name, "😀", webm_sticker = video)
+        bot.send_message(message.chat.id, f"t.me/addstickers/{pack_name}")
 
-    upload_video = open('tmp/output.webm', 'rb')
-    print(upload_video)
-    bot.create_new_sticker_set(message.from_user.id, pack_name, pack_name, "😀",  webm_sticker = upload_video)
-    bot.send_message(message.chat.id, f"t.me/addstickers/{pack_name}")
-    
 
 @bot.message_handler(func=lambda msg: True)
 def echo_all(message):
